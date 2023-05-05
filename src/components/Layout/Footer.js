3a356424897f1c46925284346b6ead67e0d3e653
Mustafa Eideh <Musageeideh.99>
2023-05-05 17:49:47 -0300

    Aula completa de React icons

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,7 +1,14 @@
+import {FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import styles from './Footer.module.css'
+
 function Footer() {
     return(
         <footer>
-            Rodapé
+            <ul className={styles.social_list}>
+                <li><FaFacebook /></li>
+                <li><FaInstagram/></li>
+                <li><FaLinkedin/></li>
+            </ul>
         </footer>
     )
 }

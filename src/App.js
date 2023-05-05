283343de7016fcbaa5146152d6c372022228e78d
Mustafa Eideh <Musageeideh.99>
2023-05-05 09:59:17 -0300

    Aula de State Lift completa

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import hook
+import { useState } from 'react';
 import './App.css';
 import SeuNome from './components/SeuNome';
+import Saudacao from './components/Saudacao';
 
 function App() {
   const[nome,setNome] = useState();
@@ -8,7 +9,8 @@ function App() {
   return (
     <div className="App">
       <h1>State Lift</h1>
-
+      <SeuNome  setNome={setNome}/>
+      <Saudacao nome={nome}/>
     </div>
   );
 }
--- src/components/SeuNome.js
+++ src/components/SeuNome.js
@@ -1,8 +1,8 @@
-function SeuNome() {
+function SeuNome({setNome}) {
     return(
         <div>
             <p>Digite seu nome:</p>
-            <input type="text" placeholder="Qual é seu nome?" />
+            <input type="text" placeholder="Qual é seu nome?" onChange={(e) => setNome(e.target.value)}/>
         </div>
     )
 }

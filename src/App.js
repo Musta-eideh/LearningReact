54ce70fc064fbd69b52f565b19d1c58f12dbe9cd
Mustafa Eideh <Musageeideh.99>
2023-05-05 10:59:14 -0300

    Primeira parte da aula de React Router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import { useState } from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import {Routes ,Route} from 'react-router-dom'
 import './App.css';
-import SeuNome from './components/SeuNome';
-import Saudacao from './components/Saudacao';
+import Home from './pages/Home';
+import Contato from './pages/Contato';
+import Empresa from './pages/Empresa';
+
 
 function App() {
-  const[nome,setNome] = useState();
 
-  return (
-    <div className="App">
-      <h1>State Lift</h1>
-      <SeuNome  setNome={setNome}/>
-      <Saudacao nome={nome}/>
-    </div>
-  );
+  // O Router envolve toda a aplicação
+  <Router>
+    <ul>
+      <li>
+        <Link to="/">Home</Link> 
+      </li>
+      <li>
+        <Link to="/empresa">Empresa</Link> 
+      </li>
+      <li>
+        <Link to="/contato">Contato</Link> 
+      </li>
+    </ul>
+
+    {/* A parte do Switch é onde substitui o componente da página */}
+
+    {/* O route efetua o roteamento, a rota "/" que pertence ao componente tal... */}
+
+    <Routes>
+      <Route path="/" element={Home}>
+        <Home />
+      </Route>
+      <Route path="/contato">
+        <Contato />
+      </Route>
+      <Route path="/empresa">
+        <Empresa />
+      </Route>
+    </Routes>
+  </Router>
 }
 
 export default App;
